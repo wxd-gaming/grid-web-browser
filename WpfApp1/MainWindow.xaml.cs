e87457e57e30193b623bd1167e389413e333352a
chenfei <492794627@qq.com>
2022-07-01 21:54:20 +0800

    初始化版本

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            CefSettings settings = new CefSettings();
+            settings.CachePath = AppDomain.CurrentDomain.BaseDirectory + "cache";
+            settings.CefCommandLineArgs.Add("disable-application-cache", "1");
+            settings.CefCommandLineArgs.Add("disable-session-storage", "1");
+            if (!Cef.IsInitialized) Cef.Initialize(settings);
+
+            InitializeComponent();
+
+            InitBrowser(2);
+
+            this.SizeChanged += new System.Windows.SizeChangedEventHandler(Window_SizeChanged);
+        }
+
+        private void Window_SizeChanged(object sender, System.EventArgs e)
+        {
+            //this.userControl.Width = this.ActualWidth / 2 - 8;
+            //this.userControl.Height = this.ActualHeight - 15;
+            //this.userControl.Resize();
+
+            //this.userContro2.Width = this.ActualWidth / 2 - 8;
+            //this.userContro2.Height = this.ActualHeight - 15;
+            //this.userContro2.Resize();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            InitBrowser(1);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            InitBrowser(2);
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            InitBrowser(4);
+        }
+
+        public void InitBrowser(int count)
+        {
+            while (this.gmain.Children.Count > count)
+            {
+                this.gmain.Children.RemoveAt(this.gmain.Children.Count - 1);
+            }
+            while (this.gmain.Children.Count < count)
+            {
+                this.gmain.Children.Add(new MyBrowser());
+            }
+        }
+
+    }
+}
---
+++ WpfApp1/MyBrowser.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyBrowser : UserControl
+    {
+
+        private ChromiumWebBrowser chromiumWebBrowser;
+
+        public MyBrowser()
+        {
+            InitializeComponent();
+            chromiumWebBrowser = new ChromiumWebBrowser();
+            this.chromiumWebBrowser.Margin = new Thickness(2, 30, 2, 2);
+            this.gmain.Children.Add(chromiumWebBrowser);
+        }
+
+        public void SetUrl(string url)
+        {
+            this.tb_url.Text = url;
+            btn_rf_Click(null, null);
+        }
+
+        private void btn_rf_Click(object sender, RoutedEventArgs e)
+        {
+            this.chromiumWebBrowser.Load(this.tb_url.Text);
+        }
+
+        private void tb_url_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.chromiumWebBrowser.Load(this.tb_url.Text);
+            }
+        }
+
+        public void SaveUrl()
+        {
+
+        }
+
+        private void btn_f12_Click(object sender, RoutedEventArgs e)
+        {
+            this.chromiumWebBrowser.ShowDevTools();
+        }
+    }
+}
